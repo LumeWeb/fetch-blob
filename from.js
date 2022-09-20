db843f859bc341557fe6e6ee296d74d2485a907f
Derrick Hammer <derrick@derrickhammer.com>
2022-09-20 15:36:21 -0400

    *Convert to cjs

--- from.js
+++ from.js
@@ -1,17 +1,17 @@
-import {
-  realpathSync,
-  statSync,
-  rmdirSync,
-  createReadStream,
-  promises as fs
-} from 'node:fs'
-import { basename, sep, join } from 'node:path'
-import { tmpdir } from 'node:os'
-import process from 'node:process'
-import DOMException from 'node-domexception'
+const {
+    realpathSync,
+    statSync,
+    rmdirSync,
+    createReadStream,
+    promises: fs
+} = require('fs')
+const { basename, sep, join } = require('path')
+const { tmpdir } = require('os')
+const process = require('process')
+const DOMException = require('node-domexception')
 
-import File from './file.js'
-import Blob from './index.js'
+const File = require('./file.js')
+const Blob = require('./index.js')
 
 const { stat, mkdtemp } = fs
 let i = 0, tempDir, registry
@@ -44,18 +44,18 @@ const fileFromSync = (path, type) => fromFile(statSync(path), path, type)
 
 // @ts-ignore
 const fromBlob = (stat, path, type = '') => new Blob([new BlobDataItem({
-  path,
-  size: stat.size,
-  lastModified: stat.mtimeMs,
-  start: 0
+    path,
+    size: stat.size,
+    lastModified: stat.mtimeMs,
+    start: 0
 })], { type })
 
 // @ts-ignore
 const fromFile = (stat, path, type = '') => new File([new BlobDataItem({
-  path,
-  size: stat.size,
-  lastModified: stat.mtimeMs,
-  start: 0
+    path,
+    size: stat.size,
+    lastModified: stat.mtimeMs,
+    start: 0
 })], basename(path), { type, lastModified: stat.mtimeMs })
 
 /**
@@ -67,16 +67,16 @@ const fromFile = (stat, path, type = '') => new File([new BlobDataItem({
  * @param {AbortSignal} [signal] in case you wish to cancel the write operation
  * @returns {Promise<Blob>}
  */
-const createTemporaryBlob = async (data, {signal, type} = {}) => {
-  registry = registry || new FinalizationRegistry(fs.unlink)
-  tempDir = tempDir || await mkdtemp(realpathSync(tmpdir()) + sep)
-  const id = `${i++}`
-  const destination = join(tempDir, id)
-  if (data instanceof ArrayBuffer) data = new Uint8Array(data)
-  await fs.writeFile(destination, data, { signal })
-  const blob = await blobFrom(destination, type)
-  registry.register(blob, destination)
-  return blob
+const createTemporaryBlob = async (data, { signal, type } = {}) => {
+    registry = registry || new FinalizationRegistry(fs.unlink)
+    tempDir = tempDir || await mkdtemp(realpathSync(tmpdir()) + sep)
+    const id = `${i++}`
+    const destination = join(tempDir, id)
+    if (data instanceof ArrayBuffer) data = new Uint8Array(data)
+    await fs.writeFile(destination, data, { signal })
+    const blob = await blobFrom(destination, type)
+    registry.register(blob, destination)
+    return blob
 }
 
 /**
@@ -90,8 +90,8 @@ const createTemporaryBlob = async (data, {signal, type} = {}) => {
  * @returns {Promise<File>}
  */
 const createTemporaryFile = async (data, name, opts) => {
-  const blob = await createTemporaryBlob(data)
-  return new File([blob], name, opts)
+    const blob = await createTemporaryBlob(data)
+    return new File([blob], name, opts)
 }
 
 /**
@@ -102,63 +102,62 @@ const createTemporaryFile = async (data, name, opts) => {
  * @private
  */
 class BlobDataItem {
-  #path
-  #start
+    #path
+    #start
 
-  constructor (options) {
-    this.#path = options.path
-    this.#start = options.start
-    this.size = options.size
-    this.lastModified = options.lastModified
-    this.originalSize = options.originalSize === undefined
-      ? options.size
-      : options.originalSize
-  }
-
-  /**
-   * Slicing arguments is first validated and formatted
-   * to not be out of range by Blob.prototype.slice
-   */
-  slice (start, end) {
-    return new BlobDataItem({
-      path: this.#path,
-      lastModified: this.lastModified,
-      originalSize: this.originalSize,
-      size: end - start,
-      start: this.#start + start
-    })
-  }
-
-  async * stream () {
-    const { mtimeMs, size } = await stat(this.#path)
-
-    if (mtimeMs > this.lastModified || this.originalSize !== size) {
-      throw new DOMException('The requested file could not be read, typically due to permission problems that have occurred after a reference to a file was acquired.', 'NotReadableError')
+    constructor (options) {
+        this.#path = options.path
+        this.#start = options.start
+        this.size = options.size
+        this.lastModified = options.lastModified
+        this.originalSize = options.originalSize === undefined
+            ? options.size
+            : options.originalSize
     }
 
-    yield * createReadStream(this.#path, {
-      start: this.#start,
-      end: this.#start + this.size - 1
-    })
-  }
+    /**
+     * Slicing arguments is first validated and formatted
+     * to not be out of range by Blob.prototype.slice
+     */
+    slice (start, end) {
+        return new BlobDataItem({
+            path: this.#path,
+            lastModified: this.lastModified,
+            originalSize: this.originalSize,
+            size: end - start,
+            start: this.#start + start
+        })
+    }
 
-  get [Symbol.toStringTag] () {
-    return 'Blob'
-  }
+    async * stream () {
+        const { mtimeMs, size } = await stat(this.#path)
+
+        if (mtimeMs > this.lastModified || this.originalSize !== size) {
+            throw new DOMException('The requested file could not be read, typically due to permission problems that have occurred after a reference to a file was acquired.', 'NotReadableError')
+        }
+
+        yield * createReadStream(this.#path, {
+            start: this.#start,
+            end: this.#start + this.size - 1
+        })
+    }
+
+    get [Symbol.toStringTag] () {
+        return 'Blob'
+    }
 }
 
 process.once('exit', () => {
-  tempDir && rmdirSync(tempDir, { recursive: true })
+    tempDir && rmdirSync(tempDir, { recursive: true })
 })
-
-export default blobFromSync
-export {
-  Blob,
-  blobFrom,
-  blobFromSync,
-  createTemporaryBlob,
-  File,
-  fileFrom,
-  fileFromSync,
-  createTemporaryFile
+exports = {
+    Blob,
+    blobFrom,
+    blobFromSync,
+    createTemporaryBlob,
+    File,
+    fileFrom,
+    fileFromSync,
+    createTemporaryFile,
+    default: blobFromSync
 }
